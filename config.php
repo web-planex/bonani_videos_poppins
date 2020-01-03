1f6297382e2a5de4ba334181e80a103d62af8087
Daksh Webplanex <dakshk@webplanex.com>
2020-01-03 10:32:15 +0530

    Fresh setup of Videos.poppin.me

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "mi_videos.poppin.me";
+
+// Site URL
+$site_url = "http://localhost/bonani/videos.poppin.me"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "a35df0f4-2232-4463-9a9f-9e7201dae1f7"; // Your purchase code, don't give it to anyone. 
+?>
